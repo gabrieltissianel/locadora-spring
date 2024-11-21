335a3417c5a477f1990bb37dc44129f7b0fcfa0f
gabriel <gabriel.gtissianel@gmail.com>
2024-11-21 17:47:23 -0300

    msm de antes

--- src/main/java/com/locadora/application/GenericApplication.java
+++ src/main/java/com/locadora/application/GenericApplication.java
@@ -3,39 +3,36 @@ package com.locadora.application;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Service;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.locadora.model.Identity;
 
 import lombok.AllArgsConstructor;
 
-@Service
 @AllArgsConstructor
-public abstract class GenericApplication<E extends Object> {
+public abstract class GenericApplication<E extends Identity> {
 
     protected final JpaRepository<E, Long> objRepository;
 
-    public List<E> list() {
-        return objRepository.findAll();
+    public ResponseEntity<List<E>> list() {
+        return ResponseEntity.ok().body(objRepository.findAll());
     }
 
-    public E add(E obj) {
-        try {
-            objRepository.save(obj);
-            return obj;
-        } catch (Exception e) {
-            return null;
-        }
+    public ResponseEntity<E> add(E obj) {
+        objRepository.save(obj);
+        return ResponseEntity.status(HttpStatus.CREATED).body(obj);
     }
 
-    public void remove(long id) {
+    public ResponseEntity<String> remove(long id) {
+        objRepository.findById(id).orElseThrow(()-> new RuntimeException("Objeto não encontrado."));
         objRepository.deleteById(id);
+        return ResponseEntity.ok().body("Registro Excluido com sucesso"); 
     }
 
-    public E update(E obj) {
-        try {
-            objRepository.save(obj);
-            return obj;
-        } catch (Exception e) {
-            return null;
-        }
+    public ResponseEntity<E> update(E obj) {
+        objRepository.findById(obj.getId()).orElseThrow(()-> new RuntimeException("Objeto não encontrado."));
+        objRepository.save(obj);
+        return ResponseEntity.ok().body(obj);
     }
 }
